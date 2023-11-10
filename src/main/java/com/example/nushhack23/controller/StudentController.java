72fa11a59d990a922de497896ce0da13b020879e
swanjax <gemsbond065@gmail.com>
2023-11-10 23:51:01 +0800

    controller classes done (correctly) waa waa herb

--- src/main/java/com/example/nushhack23/controller/StudentController.java
+++ src/main/java/com/example/nushhack23/controller/StudentController.java
@@ -1,34 +1,70 @@
 package com.example.nushhack23.controller;
 
-public class StudentController
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+
+public class StudentController {
+
         @FXML
-        private ImageView myPfp;
-        @FXML
-        private Label myName;
-        @FXML
-        private Label myId;
-        @FXML
-        private Label myHours;
-        @FXML
-        private Label myStars;
-        @FXML
-        private Label mySubjects;
+        private Button bookBtn;
+
         @FXML
         private Button editBtn;
+
         @FXML
-        private ImageView teacherPfp;
+        private Label myHours;
+
         @FXML
-        private Label teacherName;
+        private Label myId;
+
         @FXML
-        private Label teacherID;
+        private Label myName;
+
+        @FXML
+        private ImageView myPfp;
+
+        @FXML
+        private Label myStars;
+
+        @FXML
+        private Label mySubjects;
+
         @FXML
         private Label teacherHours;
+
+        @FXML
+        private Label teacherID;
+
+        @FXML
+        private Label teacherName;
+
+        @FXML
+        private ImageView teacherPfp;
+
         @FXML
         private Label teacherStars;
+
         @FXML
         private Label teacherSubjects;
+
         @FXML
         private Label teacherTimeslots;
+
         @FXML
-        private FlowPane teacherTImeslots;-viewController{
+        private FlowPane teacherTimeslotsFP;
+
+        @FXML
+        void onBook(ActionEvent event) {
+
         }
+
+        @FXML
+        void onEdit(ActionEvent event) {
+
+        }
+
+}
--- src/main/java/com/example/nushhack23/controller/TeacherController.java
+++ src/main/java/com/example/nushhack23/controller/TeacherController.java
@@ -1,40 +1,90 @@
 package com.example.nushhack23.controller;
 
-public class TeacherController
-        @FXML
-        private ImageView myPfp;
-        @FXML
-        private Label myName;
-        @FXML
-        private Label myId;
-        @FXML
-        private Label myHours;
-        @FXML
-        private Label myStars;
-        @FXML
-        private Label mySubjects;
-        @FXML
-        private Button editBtn;
-        @FXML
-        private ImageView studentPfp;
-        @FXML
-        private Label studentName;
-        @FXML
-        private Label studentID;
-        @FXML
-        private Label studentHours;
-        @FXML
-        private Label studentStars;
-        @FXML
-        private Label studentSubjects;
-        @FXML
-        private ListView listView;
-        @FXML
-        private TextField timeslotEndTF;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class TeacherController {
+
         @FXML
         private Button addTimeslotBtn;
+
+        @FXML
+        private Button editBtn;
+
+        @FXML
+        private ListView<?> listView;
+
+        @FXML
+        private Label myHours;
+
+        @FXML
+        private Label myId;
+
+        @FXML
+        private Label myName;
+
+        @FXML
+        private ImageView myPfp;
+
+        @FXML
+        private Label myStars;
+
+        @FXML
+        private Label mySubjects;
+
+        @FXML
+        private Label studentHours;
+
+        @FXML
+        private Label studentID;
+
+        @FXML
+        private Label studentName;
+
+        @FXML
+        private ImageView studentPfp;
+
+        @FXML
+        private Label studentStars;
+
+        @FXML
+        private Label studentSubjects;
+
+        @FXML
+        private TextField timeslotEndTF;
+
+        @FXML
+        private Button timeslotRemoveBtn;
+
         @FXML
         private TextField timeslotStartTF;
+
         @FXML
-        private Button timeslotRemoveBtn;-viewController{
+        void onAddTimeslot(ActionEvent event) {
+
         }
+
+        @FXML
+        void onEdit(ActionEvent event) {
+
+        }
+
+        @FXML
+        void onRemove(ActionEvent event) {
+                
+        }
+
+}
+
