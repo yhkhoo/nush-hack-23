ba51e103271b7eb47bf1ec4b7803b5d933ea947a
yhkhoo <77189596+yhkhoo@users.noreply.github.com>
2023-11-10 15:25:29 +0800

    add rating class

---
+++ src/main/java/com/example/nushhack23/model/Rating.java
@@ -0,0 +1,31 @@
+package com.example.nushhack23.model;
+
+public class Rating {
+    public double stars;
+    public String comment;
+
+    public Rating(double stars, String comment) {
+        this.stars = stars;
+        this.comment = comment;
+    }
+
+    public Rating(double stars) {
+        this(stars, "");
+    }
+
+    public double getStars() {
+        return stars;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setStars(double stars) {
+        this.stars = stars;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
