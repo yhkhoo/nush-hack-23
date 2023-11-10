56fa6252dbcc66c8d9d54839389377cdc2935a84
yhkhoo <77189596+yhkhoo@users.noreply.github.com>
2023-11-10 23:37:58 +0800

    make controllers for student and teacher

---
+++ src/main/java/com/example/nushhack23/controller/StudentController.java
@@ -0,0 +1,34 @@
+package com.example.nushhack23.controller;
+
+public class StudentController
+        @FXML
+        private ImageView myPfp;
+        @FXML
+        private Label myName;
+        @FXML
+        private Label myId;
+        @FXML
+        private Label myHours;
+        @FXML
+        private Label myStars;
+        @FXML
+        private Label mySubjects;
+        @FXML
+        private Button editBtn;
+        @FXML
+        private ImageView teacherPfp;
+        @FXML
+        private Label teacherName;
+        @FXML
+        private Label teacherID;
+        @FXML
+        private Label teacherHours;
+        @FXML
+        private Label teacherStars;
+        @FXML
+        private Label teacherSubjects;
+        @FXML
+        private Label teacherTimeslots;
+        @FXML
+        private FlowPane teacherTImeslots;-viewController{
+        }
---
+++ src/main/java/com/example/nushhack23/controller/TeacherController.java
@@ -0,0 +1,40 @@
+package com.example.nushhack23.controller;
+
+public class TeacherController
+        @FXML
+        private ImageView myPfp;
+        @FXML
+        private Label myName;
+        @FXML
+        private Label myId;
+        @FXML
+        private Label myHours;
+        @FXML
+        private Label myStars;
+        @FXML
+        private Label mySubjects;
+        @FXML
+        private Button editBtn;
+        @FXML
+        private ImageView studentPfp;
+        @FXML
+        private Label studentName;
+        @FXML
+        private Label studentID;
+        @FXML
+        private Label studentHours;
+        @FXML
+        private Label studentStars;
+        @FXML
+        private Label studentSubjects;
+        @FXML
+        private ListView listView;
+        @FXML
+        private TextField timeslotEndTF;
+        @FXML
+        private Button addTimeslotBtn;
+        @FXML
+        private TextField timeslotStartTF;
+        @FXML
+        private Button timeslotRemoveBtn;-viewController{
+        }
